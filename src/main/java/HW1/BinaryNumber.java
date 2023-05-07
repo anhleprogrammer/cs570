6510010982b632af6b4c013b2f988e06af1399f4
Anh Le <ale2@stevens.edu>
2023-05-07 16:21:11 -0400

    add binary method

---
+++ src/main/java/HW1/BinaryNumber.java
@@ -0,0 +1,126 @@
+package main.java.HW1;
+import java.util.*;
+public class BinaryNumber {
+
+    private int[] data;
+    private boolean overflow;
+
+    // Create a binary number with only 0 as values based on the input length
+    public BinaryNumber(int length) {
+        data = new int[length];
+        Arrays.fill(data, 0);
+    }
+
+    // Create binary number based on input string
+    public BinaryNumber(String str) {
+        //Create an array with length equals the length of input string
+        data = new int[str.length()];
+        //Convert character to number
+        int i = 0;
+        for(char c: str.toCharArray()) {
+            data[i] = Character.getNumericValue(c);
+            i++;
+        }
+     }
+
+     //Return the length of the binary number
+    public int getLength() {
+        return data.length;
+    }
+
+    public int getDigit(int index) {
+        // method body
+        if(index < 0 || index >= data.length) throw new IndexOutOfBoundsException("Error: index out of bounds");
+        return data[index];
+    }
+    public int toDecimal() {
+        // method body
+        StringBuilder sb = new StringBuilder();
+        for( int num: data) {
+            sb.append(String.valueOf(num));
+        }
+        return Integer.valueOf(sb.toString());
+    }
+
+    public void shiftR(int amount) {
+        // resize length of array everytime we shift
+        int[] newArr = new int[amount + data.length];
+        System.arraycopy(data,0, newArr, amount, data.length);
+        data = newArr;
+    }
+
+    public void add(BinaryNumber aBinaryNumber) {
+        // method body
+        if(aBinaryNumber.getLength() != data.length) {
+            System.out.println("Length of input binary do not coincide with this binary");
+        } else {
+            //How to add 2 binary
+            //when sum is >= 2 then sum is 10 and we keep the 0 and carry over 1
+            //this is about adding each number in 2 array together
+            //we add by going from right to left so traverse the array start at index length - 1
+            ArrayDeque<Integer> deque = new ArrayDeque<Integer>();
+            int carryOver = 0;
+            for(int i = getLength() - 1; i>= 0; i--) {
+                if(this.getDigit(i) + aBinaryNumber.getDigit(i) >= 2) {
+                    deque.addFirst(carryOver);
+                    carryOver = 1;
+                } else {
+                    if(carryOver == 1) {
+                        if(this.getDigit(i) + aBinaryNumber.getDigit(i) + carryOver < 2) {
+                            deque.addFirst(this.getDigit(i) + aBinaryNumber.getDigit(i) + carryOver);
+                            carryOver = 0;
+                        } else {
+                            deque.addFirst(0);
+                            carryOver = 1;
+                        }
+                    } else {
+                        deque.addFirst(this.getDigit(i) + aBinaryNumber.getDigit(i));
+                    }
+                }
+                if(i == 0 && carryOver == 1) deque.addFirst(1);
+            }
+            int[] result = deque.stream().mapToInt(e -> e.intValue()).toArray();
+            data = result;
+        }
+    }
+
+    //Return the decimal number as a string
+    public String toString() {
+        //The decimal number is made of the value inside the array data
+        StringBuilder sb = new StringBuilder();
+        for( int num: data) {
+            sb.append(String.valueOf(num));
+        }
+        return sb.toString();
+    }
+    public void clearOverflow() {
+        // method body
+        overflow = false;
+    }
+    public static void main(String[] args) {
+
+        BinaryNumber bn1 = new BinaryNumber(7);
+        BinaryNumber bn2 = new BinaryNumber("1011011");
+        BinaryNumber bn3 = new BinaryNumber("1111111");
+
+
+
+        BinaryNumber bn4 = new BinaryNumber("111");
+        BinaryNumber bn5 = new BinaryNumber("111");
+
+
+
+        System.out.println("Binary 1: " + bn1.toString());
+        System.out.println("Binary 2: " + bn2.toString());
+        System.out.println("Binary 3: " + bn3.toString());
+
+
+        bn1.add(bn2);
+        System.out.println("Binary 1 + Binary 2: " +bn1.toString());
+
+        bn2.add(bn3);
+
+        System.out.println("Binary 2 + Binary 3: " + bn2.toString());
+
+    }
+}
